89a6ff90c740d20dfa237f5fff687591feb984db
SIddhant7820 <siddhantghogare@gmail.com>
2026-01-16 15:09:07 +0530

    flask changes

--- app.py
+++ app.py
@@ -15,17 +15,12 @@ def test():
 def index():
     return render_template("index.html")
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method == 'GET':
-        return render_template("home.html")
-
-    try:
-        print(request.form)  # DEBUG
-
+    if request.method == 'POST':
         data = CustomData(
             gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('race_ethnicity'),
+            race_ethnicity=request.form.get('ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
@@ -37,10 +32,14 @@ def predict_datapoint():
         predict_pipeline = PredictPipeline()
         results = predict_pipeline.predict(pred_df)
 
-        return render_template("home.html", results=results[0])
+        return render_template(
+            "home.html",
+            results=round(float(results[0]), 2)
+        )
 
-    except Exception as e:
-        return f"ERROR: {str(e)}"
+    # GET request
+    return render_template("home.html")
+
     
 
 if __name__ == "__main__":
